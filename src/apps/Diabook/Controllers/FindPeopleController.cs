7099bd5b0927ad4b9e253ead15eac5226edcd53e
jenparker1975 <jenparker1975@gmail.com>
2017-03-13 09:45:07 -0400

    added more find friends methods

---
+++ src/apps/Diabook/Controllers/FindPeopleController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Diabook.Models;
+using Neo4jClient;
+using Newtonsoft.Json.Linq;
+
+namespace Diabook.Controllers
+{
+
+    public class FindPeopleController : ApiController
+    {
+        // POST api/values
+        [Route("api/person/{name}/GetFriendList")]
+        public IEnumerable<Person> Get(string name)
+        {
+            var client = new GraphClient(new Uri("http://neo4j:Temporary123@localhost:7474/db/data"));
+            client.Connect();
+            var query = client.Cypher
+                .Match("(user:Person)-[FRIENDS_WITH]-(friend:Person)")
+                .Where((Person user) => user.Name == name)
+                .Return(friend => friend.As<Person>());
+
+            var queryText = query.Query.QueryText;
+            var queryParams = query.Query.QueryParameters;
+            return query.Results;
+        }
+
+
+        // POST api/values
+        [Route("api/person/{name}/FindType1/{degreeOfSeparation}")]
+        public IEnumerable<Person> Get(string name, int degreeOfSeparation)
+        {
+            var client = new GraphClient(new Uri("http://neo4j:Temporary123@localhost:7474/db/data"));
+            client.Connect();
+            var query = client.Cypher
+                .Match(string.Format("(user:Person)-[:FRIENDS_WITH*{0}..5]->(fof)-[:DIAGNOSED_WITH]->(disease)", degreeOfSeparation))
+                .Where((Person user) => user.Name == name)
+                .Return(fof => fof.As<Person>());
+
+            var queryText = query.Query.QueryText;
+            var queryParams = query.Query.QueryParameters;
+            return query.Results;
+        }
+    }
+}
---
+++ src/apps/Diabook/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Diabook.Models
+{
+    public class Person
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("age")]
+        public int Age { get; set; }
+
+        [JsonProperty("profession")]
+        public string Profession { get; set; }
+
+    }
+}
